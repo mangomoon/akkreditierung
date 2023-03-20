de344fef7afe6b2ac4b17b4caeb6044782584dcc
Michael Shorny <michael.shorny@mangomoon.at>
2023-03-20 18:19:04 +0100

    Angebotveranwoertlich

---
+++ packages/ieb/Classes/Domain/Repository/AngebotVerantwortlichRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GeorgRinger\Ieb\Domain\Repository;
+
+
+/**
+ * This file is part of the "ieb" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * (c) 2023 Georg Ringer <mail@ringer.it>
+ */
+
+/**
+ * The repository for AngebotVerantwortliches
+ */
+class AngebotVerantwortlichRepository extends BaseRepository
+{
+}
