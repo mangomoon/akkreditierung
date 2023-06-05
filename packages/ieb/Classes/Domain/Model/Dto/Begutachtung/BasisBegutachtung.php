f34816cbf7b0e95b2934316b6be67cc1ea5de371
Georg Ringer <georg.ringer@gmail.com>
2023-06-05 22:21:30 +0200

    [TASK] Status

--- packages/ieb/Classes/Domain/Model/Dto/Begutachtung/BasisBegutachtung.php
+++ packages/ieb/Classes/Domain/Model/Dto/Begutachtung/BasisBegutachtung.php
@@ -15,13 +15,40 @@ class BasisBegutachtung extends AbstractDomainObject
     public string $reviewTotalCommentInternal = '';
     public string $reviewTotalCommentTr = '';
     public string $reviewB1CommentInternal = '';
+    public string $reviewB1CommentTr = '';
+    public int $reviewB1Status = 0;
+    public string $reviewB2CommentInternal = '';
+    public string $reviewB2CommentTr = '';
+    public int $reviewB2Status = 0;
+    public string $reviewC1CommentInternal = '';
+    public string $reviewC1CommentTr = '';
+    public int $reviewC1Status = 0;
+    public string $reviewC2CommentInternal = '';
+    public string $reviewC2CommentTr = '';
+    public int $reviewC2Status = 0;
+    public string $reviewC3CommentInternal = '';
+    public string $reviewC3CommentTr = '';
+    public int $reviewC3Status = 0;
     public int $status = 0;
+
+    private const FIELDS = [
+        'reviewTotalCommentInternal', 'reviewTotalCommentTr',
+        'reviewB1CommentInternal', 'reviewB1CommentTr', 'reviewB1Status',
+        'reviewB2CommentInternal', 'reviewB2CommentTr', 'reviewB2Status',
+        'reviewC1CommentInternal', 'reviewC1CommentTr', 'reviewC1Status',
+        'reviewC2CommentInternal', 'reviewC2CommentTr', 'reviewC2Status',
+        'reviewC3CommentInternal', 'reviewC3CommentTr', 'reviewC3Status',
+    ];
 
     public function setByAnsuchen(Ansuchen $ansuchen): void
     {
+        foreach (self::FIELDS as $field) {
+            if (!str_ends_with($field, 'Internal')) {
+                $getter = 'get' . ucfirst($field);
+                $this->$field = $ansuchen->$getter();
+            }
+        }
         $this->status = $ansuchen->getStatus();
-        $this->reviewTotalCommentTr = $ansuchen->getReviewTotalCommentTr();
-        $this->reviewTotalCommentInternal = $ansuchen->getReviewTotalCommentInternal();
     }
 
     public function copyToAnsuchen(Ansuchen $ansuchen): void
@@ -29,11 +56,21 @@ class BasisBegutachtung extends AbstractDomainObject
         if ($this->status > 0) {
             $ansuchen->setStatus($this->status);
         }
-        $ansuchen->setReviewTotalCommentTr($this->reviewTotalCommentTr);
+
+        foreach (self::FIELDS as $field) {
+            if (!str_ends_with($field, 'Internal')) {
+                $setter = 'set' . ucfirst($field);
+                $ansuchen->$setter($this->$field);
+            }
+        }
 
         // json fields
         try {
             $this->addNewComment($ansuchen, 'reviewB1CommentInternal');
+            $this->addNewComment($ansuchen, 'reviewB2CommentInternal');
+            $this->addNewComment($ansuchen, 'reviewC1CommentInternal');
+            $this->addNewComment($ansuchen, 'reviewC2CommentInternal');
+            $this->addNewComment($ansuchen, 'reviewC3CommentInternal');
         } catch (\JsonException $e) {
         }
     }
