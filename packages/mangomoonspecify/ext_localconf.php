623d88b56875579acb41438f9ed4cf148087fed4
Michael Shorny <michael.shorny@mangomoon.at>
2023-09-26 15:03:14 +0200

    TrotzdemAbschicken in AnsuchenBegutachtung Show

--- packages/mangomoonspecify/ext_localconf.php
+++ packages/mangomoonspecify/ext_localconf.php
@@ -4,4 +4,6 @@ defined('TYPO3_MODE') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['mangomoon'] = 'EXT:mangomoonspecify/Configuration/RTE/mangomoon.yaml';
 
+$GLOBALS['TYPO3_CONF_VARS']['MAIL']['layoutRootPaths']['100'] = 'EXT:mangomoonspecify/Resources/Private/SystemMail/';
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:mangomoonspecify/Configuration/TsConfig/TCEFORM.tsconfig">');
