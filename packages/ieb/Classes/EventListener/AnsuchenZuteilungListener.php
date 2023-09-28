213709c839bdbdba262a5144a92e015036824753
Georg Ringer <georg.ringer@gmail.com>
2023-09-28 20:32:28 +0200

    [TASK] Add gutachter emails #78

--- packages/ieb/Classes/EventListener/AnsuchenZuteilungListener.php
+++ packages/ieb/Classes/EventListener/AnsuchenZuteilungListener.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace GeorgRinger\Ieb\EventListener;
 
+use GeorgRinger\Ieb\Domain\Model\User;
 use GeorgRinger\Ieb\Event\AnsuchenZuteilungEvent;
 use GeorgRinger\Ieb\ExtensionConfiguration;
 use GeorgRinger\Ieb\Service\MailService;
 use GeorgRinger\Ieb\Utility\AnsuchenUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class AnsuchenZuteilungListener
 {
@@ -16,16 +18,26 @@ final class AnsuchenZuteilungListener
 
     public function __invoke(AnsuchenZuteilungEvent $event)
     {
-        $mails = AnsuchenUtility::getMailVeranwortliche($event->ansuchen);
+        $ansuchen = $event->ansuchen;
+        $mails = AnsuchenUtility::getMailVeranwortliche($ansuchen);
         $gsEmail = (new ExtensionConfiguration())->getEmailAddressGs();
         if ($gsEmail) {
             $mails[$gsEmail] = '';
         }
+        $this->addEmailFromGutachter($mails, $ansuchen->getGutachter1());
+        $this->addEmailFromGutachter($mails, $ansuchen->getGutachter2());
 
         $values = [
-            'ansuchen' => $event->ansuchen,
+            'ansuchen' => $ansuchen,
         ];
 
         $this->mailService->send('Notification/AnsuchenZuteilung', $mails, $values);
+    }
+
+    private function addEmailFromGutachter(array &$emailAddresses, ?User $user): void
+    {
+        if ($user && GeneralUtility::validEmail($user->getEmail())) {
+            $emailAddresses[$user->getEmail()] = $user->getFullName();
+        }
     }
 }
