056144fb34060bb62f01526246b78bd5cbb70cd7
Georg Ringer <georg.ringer@gmail.com>
2023-11-17 22:25:51 +0100

    [TASK] Archiv ansuchen pdf #118

---
+++ packages/ieb/Classes/ViewHelpers/FalFromRecordViewHelper.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GeorgRinger\Ieb\ViewHelpers;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Resource\FileCollector;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+class FalFromRecordViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    /**
+     * @var bool
+     */
+    protected $escapeOutput = false;
+
+    /**
+     * Initialize arguments
+     */
+    public function initializeArguments(): void
+    {
+        $this->registerArgument(
+            'table',
+            'string',
+            'Table (Foreign table)',
+            true
+        );
+        $this->registerArgument(
+            'field',
+            'string',
+            'Field (Foreign field)',
+            true
+        );
+        $this->registerArgument(
+            'id',
+            'int',
+            'ID (Foreign uid)',
+            true
+        );
+        $this->registerArgument(
+            'as',
+            'string',
+            'This parameter specifies the name of the variable that will be used for the returned ' .
+            'ViewHelper result.',
+            false,
+            'references'
+        );
+    }
+
+    /**
+     * @param array<mixed> $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        // create query builder object
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($arguments['table']);
+        // query
+        $row = $queryBuilder
+            ->select('*')
+            ->from($arguments['table'])
+            ->where($queryBuilder->expr()->eq(
+                'uid',
+                $queryBuilder->createNamedParameter($arguments['id'], \PDO::PARAM_INT)
+            ))
+            ->execute()
+            ->fetchAssociative();
+
+        $templateVariableContainer = $renderingContext->getVariableProvider();
+
+        if ($row) {
+            $fileCollector = GeneralUtility::makeInstance(FileCollector::class);
+            $fileCollector->addFilesFromRelation($arguments['table'], $arguments['field'], $row);
+
+            $templateVariableContainer->add($arguments['as'], $fileCollector->getFiles());
+        }
+
+        $content = $renderChildrenClosure();
+
+        if ($row) {
+            $templateVariableContainer->remove($arguments['as']);
+        }
+
+        return $content;
+    }
+}
