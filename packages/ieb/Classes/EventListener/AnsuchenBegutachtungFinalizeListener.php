1d7a1af2a5d796166e288e958567fe5de4561f05
Georg Ringer <georg.ringer@gmail.com>
2023-12-19 22:04:31 +0100

    [TASK] Fetch new pdf

--- packages/ieb/Classes/EventListener/AnsuchenBegutachtungFinalizeListener.php
+++ packages/ieb/Classes/EventListener/AnsuchenBegutachtungFinalizeListener.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 namespace GeorgRinger\Ieb\EventListener;
 
 use GeorgRinger\Ieb\Domain\Enum\AnsuchenStatus;
+use GeorgRinger\Ieb\Domain\Model\Ansuchen;
 use GeorgRinger\Ieb\Event\AnsuchenBegutachtungFinalizeAfterSnapshotEvent;
 use GeorgRinger\Ieb\ExtensionConfiguration;
 use GeorgRinger\Ieb\Service\MailService;
 use GeorgRinger\Ieb\Utility\AnsuchenUtility;
+use TYPO3\CMS\Core\Resource\FileReference;
+use TYPO3\CMS\Core\Resource\FileRepository;
+use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
 final class AnsuchenBegutachtungFinalizeListener
 {
@@ -49,9 +55,20 @@ final class AnsuchenBegutachtungFinalizeListener
 
         $values = [
             'ansuchen' => $event->ansuchenAfterSnapshot,
+            'pdf' => $this->getAttachmentFromAnsuchen($event->ansuchenAfterSnapshot),
             'newStatus' => $event->ansuchenAfterSnapshot->getStatus(),
         ];
 
         $this->mailService->send('Notification/AnsuchenBegutachtungFinalize', $mails, $values);
+    }
+
+    protected function getAttachmentFromAnsuchen(Ansuchen $ansuchen): ?FileReference
+    {
+        $fileRepository = GeneralUtility::makeInstance(FileRepository::class);
+        $fileObjects = $fileRepository->findByRelation('tx_ieb_domain_model_ansuchen', 'akkreditierung_pdf', $ansuchen->getUid());
+        foreach ($fileObjects as $fileObject) {
+            return $fileObject;
+        }
+        return null;
     }
 }
