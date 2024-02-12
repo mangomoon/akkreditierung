4c820111b128a725bcef57ba8f1ae2b727421bb1
Michael Shorny <michael.shorny@mangomoon.at>
2024-02-12 18:40:20 +0100

    Trägerfilter für Personensuche (Rumpf)

--- packages/ieb/Classes/Controller/PersonSearchController.php
+++ packages/ieb/Classes/Controller/PersonSearchController.php
@@ -6,6 +6,7 @@ namespace GeorgRinger\Ieb\Controller;
 use GeorgRinger\Ieb\Domain\Model\Dto\PersonSearch;
 use GeorgRinger\Ieb\Domain\Repository\BeraterRepository;
 use GeorgRinger\Ieb\Domain\Repository\TrainerRepository;
+use GeorgRinger\Ieb\Domain\Repository\ReportingRepository;
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
@@ -14,6 +15,7 @@ class PersonSearchController extends ActionController
 
     protected TrainerRepository $trainerRepository;
     protected BeraterRepository $beraterRepository;
+    protected ReportingRepository $reportingRepository;
 
     public function indexAction(PersonSearch $search = null): ResponseInterface
     {
@@ -21,10 +23,13 @@ class PersonSearchController extends ActionController
             $search = new PersonSearch();
         }
 
+        
+
         $this->view->assignMultiple([
             'search' => $search,
             'trainer' => $this->trainerRepository->findInPersonSearch($search),
             'berater' => $this->beraterRepository->findInPersonSearch($search),
+            //'tr' => $this->reportingRepository->getAllTraegerNames(),
         ]);
         return $this->htmlResponse();
     }
@@ -37,5 +42,10 @@ class PersonSearchController extends ActionController
     public function injectBeraterRepository(BeraterRepository $beraterRepository): void
     {
         $this->beraterRepository = $beraterRepository;
+    }
+
+    public function injectReportingRepository(ReportingRepository $reportingRepository): void
+    {
+        $this->reportingRepository = $reportingRepository;
     }
 }
